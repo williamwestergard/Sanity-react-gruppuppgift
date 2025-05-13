6f7fba044a12e7b4d5a458185420b37883a3d5fa
Madelen <madelennilsen98@gmail.com>
2025-05-13 11:13:29 +0200

    Made changes to App.css and App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Dinosaurier.com</h1>
-      <p>Skapad av dinosaurieexperter runt om i Sverige.</p>
-
-      <Posts />
+     <DinosaursToday />
     </>
   );
 }

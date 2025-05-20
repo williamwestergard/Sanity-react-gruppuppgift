a5f5eb8889722e8c9c41d5f2560575f6bb5991a6
williamwestergard <williamwestergardd@gmail.com>
2025-05-20 16:47:02 +0200

    responsive navbar with hamburger menu

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -23,6 +23,12 @@ const Navbar = () => {
 
     return () => window.removeEventListener("scroll", handleScroll);
   }, [lastScrollY]);
+
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setMenuOpen((prev) => !prev);
+  };
+
   return (
     <section
       id="navbar"
@@ -37,7 +43,10 @@ const Navbar = () => {
           <img src={dinoLogo} alt="Dino Logo" className="logo-img" />
         </NavLink>
       </article>
-      <article id="navbar-right-side">
+      <article
+        id="navbar-right-side"
+        style={{ display: menuOpen ? "flex" : "none" }}
+      >
         <NavLink
           to="/dinosaurs-index"
           className={({ isActive }) => (isActive ? "active-link" : "")}
@@ -103,6 +112,66 @@ const Navbar = () => {
           Guess That Dino
         </NavLink>
       </article>
+      {menuOpen ? (
+        // Close Icon
+        <svg
+          id="hamburger-menu-close-icon"
+          onClick={toggleMenu}
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            <path
+              d="M20.7457 3.32851C20.3552 2.93798 19.722 2.93798 19.3315 3.32851L12.0371 10.6229L4.74275 3.32851C4.35223 2.93798 3.71906 2.93798 3.32854 3.32851C2.93801 3.71903 2.93801 4.3522 3.32854 4.74272L10.6229 12.0371L3.32856 19.3314C2.93803 19.722 2.93803 20.3551 3.32856 20.7457C3.71908 21.1362 4.35225 21.1362 4.74277 20.7457L12.0371 13.4513L19.3315 20.7457C19.722 21.1362 20.3552 21.1362 20.7457 20.7457C21.1362 20.3551 21.1362 19.722 20.7457 19.3315L13.4513 12.0371L20.7457 4.74272C21.1362 4.3522 21.1362 3.71903 20.7457 3.32851Z"
+              fill="#ffffff"
+            />
+          </g>
+        </svg>
+      ) : (
+        // Hamburger Icon
+        <svg
+          id="hamburger-menu"
+          onClick={toggleMenu}
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          stroke="#ffffff"
+        >
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            <path
+              d="M20 7L4 7"
+              stroke="#ffffff"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+            />
+            <path
+              d="M20 12L4 12"
+              stroke="#ffffff"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+            />
+            <path
+              d="M20 17L4 17"
+              stroke="#ffffff"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+            />
+          </g>
+        </svg>
+      )}
     </section>
   );
 };
